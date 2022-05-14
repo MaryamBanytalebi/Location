cc6c908ebf1afb8dc3ce61aeb9493154615515b7
MaryamBanytlebi <Banytalebi.m@gmail.com>
2022-05-13 23:17:44 -0700

    add current and default location.

--- app/src/main/java/com/example/location/MainActivity.java
+++ app/src/main/java/com/example/location/MainActivity.java
@@ -4,45 +4,88 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    private Button btnCurrentLocation, btnAddedLocation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    //    private Button btnCurrentLocation;
     public static final int REQUEST_CODE_LOCATION = 0;
+    private FusedLocationProviderClient fusedLocationClient;
+    private GoogleMap map;
+    private Location myLocation;
+    private double mLongitude;
+    private double mLatitude;
+    private ArrayList<LatLng> locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        findViews();
-        setListeners();
+//        findViews();
+//        setListeners();
+        setUpMap();
+        defaultLocation();
+
     }
 
-    private void setListeners() {
-        btnCurrentLocation.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View view) {
-                if (hasPermission()) {
+//    private void setListeners() {
+//        btnCurrentLocation.setOnClickListener(new View.OnClickListener() {
+//            @RequiresApi(api = Build.VERSION_CODES.M)
+//            @Override
+//            public void onClick(View view) {
+//                if (hasPermission()) {
+//                    requestLocation();
+//                } else {
+//                    requestLocationPermission();
+//                }
+//            }
+//        });
+//    }
+//
+//    private void findViews() {
+//        btnCurrentLocation = findViewById(R.id.btn_current_location);
+//    }
 
-                } else {
-                    requestLocationPermission();
-                }
-            }
-        });
-    }
-
-    private void findViews() {
-        btnCurrentLocation.findViewById(R.id.btn_current_location);
-        btnAddedLocation.findViewById(R.id.btn_added_location);
+    private void setUpMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     private boolean hasPermission() {
@@ -73,18 +116,96 @@ public class MainActivity extends AppCompatActivity {
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch (requestCode){
+        switch (requestCode) {
             case REQUEST_CODE_LOCATION:
                 if (grantResults == null || grantResults.length == 0)
-                return;
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    //get location
                     return;
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    requestLocation();
+                else {
+                    Toast.makeText(this, "we do not have location permission",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
         }
     }
 
-    private void requestLocation(){
+    @SuppressLint("MissingPermission")
+    private void requestLocation() {
 
+        if (!hasPermission())
+            return;
+
+        LocationRequest locationRequest = LocationRequest.create();
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                myLocation = locationResult.getLocations().get(0);
+                Log.e("TAG", "onLocationResult: " + myLocation.getLatitude() + "," +
+                        myLocation.getLongitude());
+            }
+        };
+
+        locationRequest.setNumUpdates(1);
+        locationRequest.setInterval(0);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
     }
 
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        updateUI();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void updateUI() {
+        map.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            LatLng latLng = new LatLng(latitude, longitude);
+            map.addMarker(new MarkerOptions().position(latLng).title("موقعیت شما"));
+            map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            mLongitude = longitude;
+            mLatitude = latitude;
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(latLng)
+                    .zoom(10)
+                    .build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+        setMarkerListener();
+    }
+
+    private void setMarkerListener() {
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                map.clear();
+                MarkerOptions marker = new MarkerOptions();
+                marker.position(latLng);
+                marker.title("موقعیت انتخابی شما");
+                mLongitude = marker.getPosition().longitude;
+                mLatitude = marker.getPosition().latitude;
+                map.addMarker(marker);
+
+            }
+        });
+    }
+
+    private void defaultLocation() {
+        if (locations != null) {
+            locations.add(new LatLng(35.759131, 51.333236));
+            locations.add(new LatLng(35.757785, 51.327323));
+            locations.add(new LatLng(35.757815, 51.320967));
+        }
+    }
 }
